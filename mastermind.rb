fddb07cb8757616ebc2b01797b02b01abc07f916
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-05-16 16:11:41 +0530

    Initial commit
    Ruby project exercise of the ODIN project

--- mastermind.rb
+++ mastermind.rb
@@ -1,26 +1,36 @@
 color_pegs = %w[orange blue red green yellow purple]
-hint_pegs = []
-hints = []
 secret_code = []
+hints_array = []
+hints = []
 
 (0..3).each { |i| secret_code[i] = color_pegs[rand(6)] }
-# secret_code_copy = secret_code
-
-until hints.length == 4 && hints.all?('red')
+# yellow blue purple red
+p secret_code
+until hints.length == 4 && hints.all?('black')
+  store = Hash.new { |hash, key| hash[key] = [] }
+  hints = []
   puts 'Enter your guesses in 1234 order separated by a space'
   guess_array = gets.chomp.split
-  p guess_array
-  guess_array.each_with_index do |guess, idx|
-    next unless secret_code.include?(guess)
+  guess_array.each_index do |guess_index|
+    next unless secret_code.include?(guess_array[guess_index])
 
-    guess == secret_code[idx] ? hints.push('black') : hints.push('white')
-    # secret_code_copy.shift
+    matched_code_index = secret_code.each_index.select { |i| secret_code[i] == guess_array[guess_index] }
+    store[matched_code_index] = store[matched_code_index].push(guess_index)
   end
-  hint_pegs.push(hints.shuffle)
-  hints = []
-
-  p hint_pegs
-  p secret_code
-  # p secret_code_copy
-
+  store.each_pair do |matched_code_index, matched_guess_index|
+    iter_array = matched_code_index.size <= matched_guess_index.size ? matched_code_index : matched_guess_index
+    search_array = matched_code_index.size <= matched_guess_index.size ? matched_guess_index : matched_code_index
+    iter_array.each do |index_value|
+      search_array.include?(index_value) ? hints.push('black') : hints.push('white')
+    end
+  end
+  p store
+  p hints.shuffle
+  p hints.shuffle unless hints.length == 4 && hints.all?('black')
+  hints_array.push(hints.shuffle)
+  if hints_array.length > 12
+    p 'You loose'
+    break
+  end
 end
+p "You won! the correct code is #{secret_code}"
